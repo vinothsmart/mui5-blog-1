15069ff393de7b5b6114c92d8e0d7f2d7d8218e9
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-14 15:54:09 +0530

    Typography added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 
 function App() {
   return (
     <div className="App">
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
       <Button endIcon={<FacebookIcon />} variant="outlined" color="secondary">
         Text
       </Button>
