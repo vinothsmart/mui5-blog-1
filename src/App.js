71eb131efc66670a416a668e57a2752e0846a280
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-14 19:28:12 +0530

    Stack added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import { Container } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
-    <Container
+    <Stack
+      direction="row"
       sx={{
         backgroundColor: "tomato",
       }}
     >
-      Hello World
-    </Container>
+      <Box
+        flex={1}
+        sx={{
+          backgroundColor: "tomato",
+        }}
+      >
+        Box1
+      </Box>
+      <Box
+        flex={5}
+        sx={{
+          backgroundColor: "blue",
+        }}
+      >
+        Box2
+      </Box>
+      <Box
+        flex={2}
+        sx={{
+          backgroundColor: "green",
+        }}
+      >
+        Box3
+      </Box>
+    </Stack>
   );
 }
 
