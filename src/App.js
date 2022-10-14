c1cb4a0a78e9b97292650774ec175c3b6815ce24
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-14 16:05:48 +0530

    Box used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { Button, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 
 function App() {
-  return (
-    <div className="App">
-      <Typography
-        align="center"
-        variant="h1"
-        sx={{
-          backgroundColor: "red",
-          color: "white",
-          fontSize: 12,
-        }}
-      >
-        h1. Heading
-      </Typography>
-      <Button endIcon={<FacebookIcon />} variant="outlined" color="secondary">
-        Text
-      </Button>
-    </div>
-  );
+  return <Box>Hello World</Box>;
 }
 
 export default App;
