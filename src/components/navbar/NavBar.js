a0bda99f828b4ae42a7f1399b4c6e440dbfefdd6
vinothsmart <s.vinothkanna90@gmail.com>
2024-08-13 17:44:27 +0530

    Refactor NavBar component with styled components and add social icons, menu items, and search box

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,16 +1,63 @@
-import { AppBar, Box, styled, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 
 const NavBar = () => {
   const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
   });
+
+  const StyledBox = styled(Box)({
+    display: "flex",
+    gap: 10,
+  });
+
+  const MenuBox = styled(Box)({
+    display: "flex",
+    gap: 10,
+  });
+
+  const MenuItems = [
+    { Name: "Home", Link: "/" },
+    { Name: "Products", Link: "#" },
+    { Name: "Portfolio", Link: "#" },
+    { Name: "Blog", Link: "#" },
+    { Name: "Contact Us", Link: "#" },
+  ];
+
+  const SearchBox = styled(Box)({
+    display: "flex",
+    gap: 5,
+  });
+
   return (
-    <AppBar>
+    <AppBar sx={{ background: "black" }}>
       <StyledToolbar>
-        <Box>Social</Box>
-        <Box>Menu</Box>
-        <Box>Searh</Box>
+        <StyledBox>
+          <Facebook />
+          <Instagram />
+          <Twitter />
+        </StyledBox>
+        <MenuBox>
+          {MenuItems.map((o) => (
+            <Typography
+              key={o.Name}
+              sx={{ cursor: "pointer", fontSize: "14px" }}
+            >
+              {o.Name}
+            </Typography>
+          ))}
+        </MenuBox>
+        <SearchBox>
+          <InputBase placeholder="Search ..." sx={{ color: "white" }} />
+        </SearchBox>
       </StyledToolbar>
     </AppBar>
   );
