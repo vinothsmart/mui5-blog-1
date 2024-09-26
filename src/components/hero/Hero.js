b65612baf3c3b4af07b4ac677a64d1f9715b0025
vinothsmart <s.vinothkanna90@gmail.com>
2024-09-26 10:05:47 +0530

    Refactor Hero component, add background image and content

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,6 @@
 import React, { memo } from "react";
 import { Box, Typography } from "@mui/material";
+import HeroImage from "../../static/HeroImage.jpg";
 
 const Hero = () => {
   return (
@@ -10,7 +11,38 @@ const Hero = () => {
       <Typography variant="body2" align="center" sx={{ fontWeight: 900 }}>
         We make you look the better of you!
       </Typography>
-      {/* <Box sx={{ backgroundImage: `url()` }}></Box> */}
+      <Box
+        sx={{
+          backgroundImage: `url(${HeroImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "black",
+          backckgroundPosition: "center",
+          backgroundSize: "cover",
+          height: 600,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            width: { xs: "100%", sm: "100%", md: "40%" },
+            padding: { xs: 3, sm: 2, md: 20 },
+          }}
+        >
+          <Box sx={{ backgroundColor: "white", opacity: "0.8" }}>
+            <Typography variant="h6" color="tomato" align="center" pt={8}>
+              Trending Styles
+            </Typography>
+            <Typography variant="h4" align="center">
+              Life is boring without Fashion!
+            </Typography>
+            <Typography variant="body1" align="center" pb={8}>
+              We love fashion and we love you!
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
