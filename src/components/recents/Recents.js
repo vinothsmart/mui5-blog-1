45ef0f6157fb82b5c43389fd604e7e42b1c8786a
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-02 07:48:44 +0530

    up

--- src/components/recents/Recents.js
+++ src/components/recents/Recents.js
@@ -1,4 +1,17 @@
+import { Box, Grid } from "@mui/material";
+
 const Recents = () => {
-  return <div>Recents</div>;
+  return (
+    <Box>
+      <Grid container>
+        <Grid item md={6} xs={12}>
+          Item1
+        </Grid>
+        <Grid item sx={{ backgroundColor: "lightgreen" }}>
+          Item2
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 export default Recents;
