4992774d11d64c5d49733df452578bf332c8edb4
vinothsmart <s.vinothkanna90@gmail.com>
2025-08-08 11:28:29 +0530

    Refactor Recents component to include CardMedia, CardContent, and CardActions for improved layout and content display

--- src/components/recents/Recents.js
+++ src/components/recents/Recents.js
@@ -1,11 +1,39 @@
-import { Box, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+import cardImage from "../../static/image3.jpg";
 
 const Recents = () => {
   return (
     <Box>
       <Grid container>
         <Grid item md={6} xs={12}>
-          Item1
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image={cardImage}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
         </Grid>
         <Grid item sx={{ backgroundColor: "lightgreen" }}>
           Item2
